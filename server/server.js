efa40f86074f0eec9c02a6b02a439dc21605ee3a
ADY <ujudge@naver.com>
2024-05-21 11:27:27 +0900

    mongoDB 연결 테스트

--- server/server.js
+++ server/server.js
@@ -1,10 +1,44 @@
 // server/server.js
+
+
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// MongoDB 연결
+const uri = `mongodb+srv://${process.env.MONGODB_URI_USERNAME}:${process.env.MONGODB_URI_PASSWORD}@users.nn35fcc.mongodb.net/?retryWrites=true&w=majority&appName=users`;
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
+
+// 스키마 및 모델
+const itemSchema = new mongoose.Schema({
+    name: String,
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+// 라우트
+app.get('/api/items', async (req, res) => {
+    const items = await Item.find();
+    res.json(items);
+});
+
+app.post('/api/items', async (req, res) => {
+    const newItem = new Item(req.body);
+    await newItem.save();
+    res.json(newItem);
+});
 
 console.log("Server running on port:", PORT);
 
